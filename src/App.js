c0e0be98591c56589c725b7c2e1571535d9751ed
GoodnessObi <goodnessobi@gmail.com>
2021-07-09 18:39:32 +0100

    exploring hooks - usestate and useeffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
+import SongList from "./components/SongList";
+
 function App() {
   return (
     <div className="App">
-
+      <SongList />
     </div>
   );
 }
